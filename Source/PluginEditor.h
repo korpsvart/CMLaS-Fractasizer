a980a21fc3cbadd65b10370e50fc524ae3730b50
korpsvart <riccardodibella9@gmail.com>
2022-04-10 17:24:41 +0200

    Initial commit - setting up JUCE project

---
+++ Source/PluginEditor.h
@@ -0,0 +1,33 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class FractalSynthesisAudioProcessorEditor  : public juce::AudioProcessorEditor
+{
+public:
+    FractalSynthesisAudioProcessorEditor (FractalSynthesisAudioProcessor&);
+    ~FractalSynthesisAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    FractalSynthesisAudioProcessor& audioProcessor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (FractalSynthesisAudioProcessorEditor)
+};
