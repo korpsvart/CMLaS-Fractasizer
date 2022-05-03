5e874d4374b8bf1209dd5bf1da2e6fbcd9aeb51f
juanibraun <juanibraun@gmail.com>
2022-05-03 20:46:24 +0200

    Made the basic layout of the plugin, still have to link the sliders with the proccessor

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -38,15 +38,29 @@ private:
 
     juce::Slider initialPointXSlider;
     juce::Slider initialPointYSlider;
+    
+    juce::Slider attackSlider;
+    juce::Slider sustainSlider;
+    juce::Slider decaySlider;
+    juce::Slider releaseSlider;
+    
+    juce::Slider numPartialsSlider;
 
     InputPlane inputPlaneComponent;
+    
+    juce::ImageComponent mImageComponent;
 
     //Attachments (must be declared after the GUI elements to avoid crashes when closing the plugin)
     std::unique_ptr<juce::AudioProcessorValueTreeState::ComboBoxAttachment> fractalComboBoxAttachment;
 
     std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> sliderInitialPointXAttachment;
     std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> sliderInitialPointYAttachment;
-
+    
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> attackAttachment;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> sustainAttachment;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> decayAttachment;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> releaseAttachment;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> numPartialsAttachment;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (FractalSynthesisAudioProcessorEditor)
 };
